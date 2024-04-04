b0f41423171eeabe6ef14185bf678cd6d9769eb2
Bueze <chibuezeth@gmail.com>
2024-04-04 14:34:03 +0100

    Completed responsive on search bar

--- JS/navbar.js
+++ JS/navbar.js
@@ -1,8 +1,23 @@
 const search = document.querySelector('.search-bar i');
 const searchBar = document.querySelector('.search-bar input');
+const searchInput = document.querySelector('.search-bar .search-input');
+
+let clicked = false;
 
 search.addEventListener("click", ()=>{
     console.log("search");
-    searchBar.classList.remove('nav-hide');
-    searchBar.classList.add('show-search-bar');
+
+    if(clicked) {
+        searchBar.classList.add('nav-hide');
+        searchBar.classList.remove('show-search-bar');
+        searchInput.classList.remove("search");
+        search.classList.remove("search-color");
+        clicked = false;
+    } else  {
+        searchBar.classList.remove('nav-hide');
+        searchBar.classList.add('show-search-bar');
+        searchInput.classList.add("search");
+        search.classList.add("search-color");
+        clicked = true;
+    }
 })
